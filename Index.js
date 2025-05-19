519f28555efe2a7e5e4da479660275548e881573
P Bhosale <prathmeshm.bhosale@gmail.com>
2025-05-19 14:41:38 +0530

    Delpoy all

---
+++ Index.js
@@ -0,0 +1,108 @@
+$('.toggle_btn').click(function() {
+$('.toggle_btn i').removeClass('fa-solid fa-bars');
+$('.toggle_btn i').addClass('fa-solid fa-xmark');
+});
+
+
+
+// var toggle = document.querySelector(".toggle-btn")
+// var togggleicon = document.querySelector(".toggle-btn i")
+// var togglemenu = document.querySelector(".menus")
+
+// toggle.onclick = function () {
+//     togglemenu.classList.toggle('open')
+//     const isOpen = togglemenu.classList.contains('open')
+
+//     togggleicon.classList = isOpen ?
+//         // 'fa-solid fa-xmark' 
+//         'fa-solid fa-xmark' :
+//         'fa-solid fa-bars'
+// }
+
+// function click(){
+//     toggle.onclick ={
+//     }
+// }
+
+
+
+// function change(){
+
+//     if (onclick == once)
+//     document.getElementById("i").style.display = "none";
+//     document.getElementById("i").innerHTML  = `<button>
+//                     <i class="fa-solid fa-xmark nav-x" id="x"></i>
+//                 </button>`;
+// var toggle = document.getElementById("toggle")
+// var nav = document.getElementById("navbar")
+// if (nav === true){
+//     document.getElementById("toggle").style.visibility = "hidden";
+// }
+// }
+
+
+
+
+// $(document).on('click', '#toggle', function() {
+//   $('.nav-togg').animate({display:'none'}, 100);
+// });
+
+// $(document).on('click', '.toggled-on', function() {
+//   $('.site-content').animate({opacity:0, visibility:'visible'},        300);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function questionshow(){
+//     document.getElementById('question').style.display = 'block';
+// }
+
+// function questionshow(){
+//     var que = document.getElementById("question");
+//     if(que.style.display==='none'){
+//         que.style.display='block'
+//         document.getElementById('ques1').style.color = '#1acc8d';
+//     }else{
+//         que.style.display='none'
+//         document.getElementById('ques1').style.color = '#040677';
+//     }
+// }
+// function questionshow2(){
+//     var que1 = document.getElementById("question2");
+//     if(que1.style.display==='none'){
+//         que1.style.display='block'
+//         document.getElementById('ques2').style.color = '#1acc8d';
+//     }else{
+//         que1.style.display='none'
+//         document.getElementById('ques2').style.color = '#040677';
+//     }
+// }
+// function questionshow3(){
+//     var que2 = document.getElementById("question3");
+//     if(que2.style.display==='none'){
+//         que2.style.display='block'
+//         document.getElementById('ques3').style.color = '#1acc8d';
+//     }else{
+//         que2.style.display='none'
+//         document.getElementById('ques3').style.color = '#040677';
+//     }
+// }
